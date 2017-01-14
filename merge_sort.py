d8121ab15e3a3bd3fe042f39858d70205423a471
Vayne-Lover <406378362@qq.com>
2017-01-14 21:54:15 +0800

    Fix bugs.

--- merge_sort.py
+++ merge_sort.py
@@ -1,16 +1,12 @@
 #!/usr/local/bin/python
-from math import floor
 
-def merge(a,p,q,r):
-  if p==r
-    return a
-  else:
-    new1=a[p:q+1]+[float('inf')]
-    new2=a[q+1:r]+[float('inf')]
+def merge(a,b):
+    result=[]
+    new1=a[:]+[float('inf')]
+    new2=b[:]+[float('inf')]
     i=0
     j=0
-    result=[]
-    for k in range(r-p+1):
+    for k in range(0,len(a)+len(b)):
       if new1[i]<=new2[j]:
         result.append(new1[i])
         i+=1
@@ -19,12 +15,22 @@ def merge(a,p,q,r):
         j+=1
     return result
 
-def merge_sort(a,p,r):
-  if  p<r:
-    q=floor((p+r)/2)
-    merge_sort(a,p,q)
-    merge_sort(a,q+1,r)
-    merge(a,p,q,r)
+def merge_sort(a):
+  if  len(a)<=1:
+    return a
+  else:
+    mid=len(a)/2
+    left=merge_sort(a[:mid])
+    right=merge_sort(a[mid:])
+    return merge(left,right)
 
-
-  
+if __name__=='__main__':
+    t1=[15,4,8,2,3,6,12,24]
+    t2=[-2,1,-9.3,5.8,9,10,7]
+    t3=[]
+    print merge_sort(t1)
+    print t1
+    print merge_sort(t2)
+    print t2
+    print merge_sort(t3)
+    print t3
