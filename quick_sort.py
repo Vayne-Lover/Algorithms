d1d473e622580b95b29049891bdfaf2d45a40c2f
Vayne-Lover <406378362@qq.com>
2017-01-21 12:43:44 +0800

    Add three median quick sort.

--- quick_sort.py
+++ quick_sort.py
@@ -17,6 +17,25 @@ def random_partition(A,p,r):
   A[r],A[i]=A[i],A[r]
   return  partition(A,p,r)
 
+def three_median_partition(A,p,r):
+  (a,b,c)=[random.randint(p,r) for _ in range(3)]
+  if a>=b:
+    if c>=a:
+      mid=a
+    elif c>=b:
+      mid=c
+    else:
+      mid=b
+  else:
+    if c>=b:
+      mid=b
+    elif c>=a:
+      mid=c
+    else:
+      mid=a
+  A[mid],A[r]=A[r],A[mid]
+  return partition(A,p,r)
+
 def quick_sort(A,p,r):
   if p<r:
     i=partition(A,p,r)
@@ -29,8 +48,25 @@ def random_quick_sort(A,p,r):
     random_quick_sort(A,p,i-1)
     random_quick_sort(A,i+1,r)
 
+def three_median_quick_sort(A,p,r):
+  if p<r:
+    i=three_median_partition(A,p,r)
+    three_median_quick_sort(A,p,i-1)
+    three_median_quick_sort(A,i+1,r)
+
 if __name__=='__main__':
   A=[2,4,6,3,8,7,9]
+  B=[2,2,5,7,-2,7,-4,3,9.2]
+  C=[]
   #quick_sort(A,0,len(A)-1)
-  random_quick_sort(A,0,len(A)-1)
+  #quick_sort(B,0,len(B)-1)
+  #quick_sort(C,0,len(C)-1)
+  #random_quick_sort(A,0,len(A)-1)
+  #random_quick_sort(B,0,len(B)-1)
+  #random_quick_sort(C,0,len(C)-1)
+  three_median_quick_sort(A,0,len(A)-1)
+  three_median_quick_sort(B,0,len(B)-1)
+  three_median_quick_sort(C,0,len(C)-1)
   print A
+  print B
+  print C
