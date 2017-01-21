d0b5463c5c8f1dd78f30491805448e4c7f54f180
Vayne-Lover <406378362@qq.com>
2017-01-21 12:32:30 +0800

    Add random quick sort.

--- quick_sort.py
+++ quick_sort.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
+import random
 
 def partition(A,p,r):
   x=A[r]
@@ -11,13 +12,25 @@ def partition(A,p,r):
   A[i+1],A[r]=A[r],A[i+1]
   return i+1
 
+def random_partition(A,p,r):
+  i=random.randint(p,r)
+  A[r],A[i]=A[i],A[r]
+  return  partition(A,p,r)
+
 def quick_sort(A,p,r):
   if p<r:
     i=partition(A,p,r)
     quick_sort(A,p,i-1)
     quick_sort(A,i+1,r)
 
+def random_quick_sort(A,p,r):
+  if p<r:
+    i=random_partition(A,p,r)
+    random_quick_sort(A,p,i-1)
+    random_quick_sort(A,i+1,r)
+
 if __name__=='__main__':
   A=[2,4,6,3,8,7,9]
-  quick_sort(A,0,len(A)-1)
+  #quick_sort(A,0,len(A)-1)
+  random_quick_sort(A,0,len(A)-1)
   print A
